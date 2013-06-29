619189208b599ce79d594037d649a49a94440cca
Rob Johnstone <rjohnstone@pubcomplete.com>
2013-06-30 01:10:23 +0200

    Tidied up object inheritance

--- js/baddyPrototype.js
+++ js/baddyPrototype.js
@@ -1,54 +1,57 @@
-var baddyPrototype = $.extend(Object.create(actorPrototype), {
-	firerId: (function() {
-		var counter=-1;
-		return (function() {
-			return counter++;
-		})();
-	})(),
-	target: {x: 0, y: 0},
-	process: function() {
-		if (player.health <= 0) this.mode = 'watch';
-		else {
-			if (this.baddySeePlayer()) {
-				this.targetTile = map.getTileIndex(player);
-				this.target = map.getTileCentre(this.targetTile);
-				this.target.direction = vector.clone(player.direction);
-				this.targetDistance = vector.distance(this, this.target);
-				this.fire(player);
-				if(this.targetDistance > this.idealRange) {
-					this.mode = 'chase';
-				}
-				else {
-					this.mode = 'attack';
-				}
+var baddyPrototype = Object.create(actorPrototype);
+
+baddyPrototype.firerId = (function() {
+	var counter=-1;
+	return (function() {
+		return counter++;
+	})();
+})();
+
+baddyPrototype.target = {x: 0, y: 0};
+
+baddyPrototype.process = function() {
+	if (player.health <= 0) this.mode = 'watch';
+	else {
+		if (this.baddySeePlayer()) {
+			this.targetTile = map.getTileIndex(player);
+			this.target = map.getTileCentre(this.targetTile);
+			this.target.direction = vector.clone(player.direction);
+			this.targetDistance = vector.distance(this, this.target);
+			this.fire(player);
+			if(this.targetDistance > this.idealRange) {
+				this.mode = 'chase';
 			}
-			switch (this.mode) {
-				case 'watch':
-					// currently do nothing
-					break;
-				case 'attack':
-					this.direction = vector.subtract(this, this.target);
-					if (player.health > 0 && !this.baddySeePlayer()) {
-						this.mode = 'chase';
-					}
-					break;
-				case 'chase':
-					if (this.targetDistance < this.speed) {
-						this.x = this.target.x;
-						this.y = this.target.y;
-						this.mode = 'watch';
-						this.direction = this.target.direction;
-					}
-					else {
-						this.moveTo(this.target);
-					}
-					break;
+			else {
+				this.mode = 'attack';
 			}
 		}
-	},
-	baddySeePlayer: function() {
-		var targetVector = vector.subtract(this, player);
-		if (vector.mag(targetVector) > this.maxRange || vector.dot(this.direction, targetVector) < 0) return false;
-		return map.lineTraversable(this, player);
+		switch (this.mode) {
+			case 'watch':
+				// currently do nothing
+				break;
+			case 'attack':
+				this.direction = vector.subtract(this, this.target);
+				if (player.health > 0 && !this.baddySeePlayer()) {
+					this.mode = 'chase';
+				}
+				break;
+			case 'chase':
+				if (this.targetDistance < this.speed) {
+					this.x = this.target.x;
+					this.y = this.target.y;
+					this.mode = 'watch';
+					this.direction = this.target.direction;
+				}
+				else {
+					this.moveTo(this.target);
+				}
+				break;
+		}
 	}
-});
+};
+
+baddyPrototype.baddySeePlayer = function() {
+	var targetVector = vector.subtract(this, player);
+	if (vector.mag(targetVector) > this.maxRange || vector.dot(this.direction, targetVector) < 0) return false;
+	return map.lineTraversable(this, player);
+};
--- js/projectilePrototype.js
+++ js/projectilePrototype.js
@@ -1,34 +1,33 @@
-var projectilePrototype  = $.extend(Object.create(entityPrototype), {
-	entityType: 'projectile',
-	process: function() {
-		this.move();
-	},
-	move: function() {
-		var tile,
-			projectile = this;
-		this.x += this.speed.x * timer.coeff;
-		this.y += this.speed.y * timer.coeff;
-		// check for collisions with tiles
-		tile = map.getTileIndex(this);
-		if (map.data[tile] === undefined || !tiles.tileset[map.data[tile]].passable) {
-			this.remove = true;
-		}
-		// check for collisions with actors
-		entities.instances.forEach(function(entity) {
-			if (entity.entityType !== 'projectile' && entity.entityId !== projectile.firerId && entity.x - entity.halfWidth < projectile.x && entity.x + entity.halfWidth >projectile.x && entity.y - entity.halfHeight < projectile.y && entity.y + entity.halfHeight >projectile.y) {
-				projectile.remove = true;
-				if (typeof entity.hit === 'function') {
-					entity.hit(projectile);
-				}
-			}
-		});
-	},
-	render: function() {
-		var x = map.offset.x + this.x,
-			y = map.offset.y + this.y;
-		graphics.gameContext.beginPath();
-		graphics.gameContext.arc(x, y, this.halfWidth, 0, 2 * Math.PI);
-		graphics.gameContext.fillStyle = 'yellow';
-		graphics.gameContext.fill();
+var projectilePrototype  = Object.create(entityPrototype);
+projectilePrototype.entityType = 'projectile';
+projectilePrototype.process = function() {
+	this.move();
+};
+projectilePrototype.move = function() {
+	var tile,
+		projectile = this;
+	this.x += this.speed.x * timer.coeff;
+	this.y += this.speed.y * timer.coeff;
+	// check for collisions with tiles
+	tile = map.getTileIndex(this);
+	if (map.data[tile] === undefined || !tiles.tileset[map.data[tile]].passable) {
+		this.remove = true;
 	}
-});
+	// check for collisions with actors
+	entities.instances.forEach(function(entity) {
+		if (entity.entityType !== 'projectile' && entity.entityId !== projectile.firerId && entity.x - entity.halfWidth < projectile.x && entity.x + entity.halfWidth >projectile.x && entity.y - entity.halfHeight < projectile.y && entity.y + entity.halfHeight >projectile.y) {
+			projectile.remove = true;
+			if (typeof entity.hit === 'function') {
+				entity.hit(projectile);
+			}
+		}
+	});
+};
+projectilePrototype.render = function() {
+	var x = map.offset.x + this.x,
+		y = map.offset.y + this.y;
+	graphics.gameContext.beginPath();
+	graphics.gameContext.arc(x, y, this.halfWidth, 0, 2 * Math.PI);
+	graphics.gameContext.fillStyle = 'yellow';
+	graphics.gameContext.fill();
+};
