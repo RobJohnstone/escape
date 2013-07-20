5a10b6ec125f0b6e88e46d1f7c31d806a2e669cd
Rob Johnstone <rjohnstone@pubcomplete.com>
2013-07-20 23:35:38 +0200

    Added support for campaigns

---
+++ client/js/playerPrototype.js
@@ -0,0 +1,35 @@
+/**
+ * Prototype for the player. Inherits from actor prototype
+ *
+ * For more information on how Escape uses inheritance see objectPrototype.js
+ *
+ * @module playerPrototype
+ * @class playerPrototype
+ * @extends actorPrototype
+ */
+E.playerPrototype = (function() {
+	"use strict";
+
+	return E.actorPrototype.extend({
+		width: 32,
+		height: 32,
+		colour: 'green',
+		speed: 5,
+		direction: {x: 1, y: 0},
+		invulnerable: true,
+		/**
+		 * Actions to be taken each frame for the player
+		 *
+		 * @method process
+		 * @return this
+		 */
+		process: function() {
+			var tileIndex = E.map.getTileIndex(this);
+			if (E.map.getTileObj(tileIndex).exit) {
+				//E.game.levelComplete();
+				E.game.mode = 'levelComplete';
+			}
+			return this;
+		}
+	});
+})();
